e4dc28feca8dbc2ca7e30e6ba97a148c6c961b83
Steph974-Git <10466@holbertonstudents.com>
2025-08-12 23:17:18 +0200

    Remove unnecessary return statement from update_topics function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,7 +5,6 @@
 def update_topics(mongo_collection, name, topics):
     """
     Updates the topics of a school document based on the name.
-    Returns the number of documents modified.
     """
     result = mongo_collection.update_one(
         {"name": name},
