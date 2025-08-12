1744af55f5774e8771e8841ae229fe6b153fe032
Steph974-Git <10466@holbertonstudents.com>
2025-08-12 22:02:07 +0200

    Add docstring to list_all function for clarity

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def list_all(mongo_collection):
-    
+    """
+    Lists all documents in a MongoDB collection.
+    """
+    document = mongo_collection.find()
+    return list(document)
