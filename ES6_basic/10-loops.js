e742dc1443ffe701fe4af3677dd9b3104d001c11
Steph974-Git <10466@holbertonstudents.com>
2025-08-18 15:56:21 +0200

    refactor: change variable declarations from let to const in taskBlock function and update appendToEachArrayValue to return a new array

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+  let result = [];
+  for (const value of array) {
+    result.push(appendString + value);
   }
-  return array;
+
+  return result;
 }
