0eb6b4680ba81b6d735108724aba05c31ba66975
Steph974-Git <10466@holbertonstudents.com>
2025-08-18 15:55:29 +0200

    refactor: replace var with const in taskBlock function and simplify loop in appendToEachArrayValue function

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx = 0; idx < array.length; idx++) {
-    array[idx] = appendString + array[idx];
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value;
   }
   return array;
 }
