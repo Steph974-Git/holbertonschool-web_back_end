b2308568cd15a3346569fc5fccaefec3f7081f49
Steph974-Git <10466@holbertonstudents.com>
2025-08-19 09:50:42 +0200

    refactor: fix getNumberOfDepartments method to use allEmployees for department count

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject (employeesList) {
   return {
     allEmployees: {...employeesList},
     getNumberOfDepartments () {
-      return Object.keys(this.employeesList).length;
-    }
+      return Object.keys(this.allEmployees.length);
+    },
   };
 }
