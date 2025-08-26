e2d4db0c1787040ee04df6e2a0ee0f79b9083703
Steph974-Git <10466@holbertonstudents.com>
2025-08-26 15:40:50 +0200

    feat: implement handleResponseFromAPI and handleProfileSignup functions with promise handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,3 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  
+  return new Promise((resolve, reject) => {
+    promise.then(result => resolve({ status: 200, body: 'Success' }))
+      .catch(error => reject(error)) .finally(console.log("Got a response from the API"));
+  });
 }
---
+++ ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import uploadPhoto, { createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(())
+
+  })
+}
