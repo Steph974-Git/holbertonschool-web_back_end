71d8eabdd74274b6fe34240c83cb86aa27938cf9
Steph974-Git <10466@holbertonstudents.com>
2025-08-26 10:19:21 +0200

    feat: add setFromArray and hasValuesFromArray functions with main files for testing

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string')
+    return "";
+  const Barray = Array.from(set)
+
+  set.startsWith("bon");
+  set.slice(3);
+
+}
