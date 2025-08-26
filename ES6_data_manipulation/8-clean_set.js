523ddc46868c00356442f7dd32de8f3324e7c5eb
Steph974-Git <10466@holbertonstudents.com>
2025-08-26 11:14:01 +0200

    feat: enhance cleanSet function to validate startString type and add README file

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString === '')
+  if (typeof startString !== 'string' || startString.length === 0) {
     return "";
+  }
   return [...set]
-    .filter(value => value.startsWith(startString))
+    .filter(value => typeof value === 'string' && value.startsWith(startString))
     .map(value => value.slice(startString.length))
     .join('-');
 }
