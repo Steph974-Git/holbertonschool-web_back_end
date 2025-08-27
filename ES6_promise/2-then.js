1fa7e8ea04181de8a4835c4d95933ed69627a717
Steph974-Git <10466@holbertonstudents.com>
2025-08-27 11:16:19 +0200

    feat: refactor handleResponseFromAPI and handleProfileSignup functions for improved promise handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then(result => resolve({ status: 200, body: 'Success' }))
-      .catch(error => reject(error)) .finally(console.log("Got a response from the API"));
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
