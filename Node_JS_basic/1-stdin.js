97bff99c1c45900e85ecb19d729726a29976092a
Steph974-Git <10466@holbertonstudents.com>
2025-09-01 10:47:53 +0200

    feat: add console and stdin interaction files, and update package.json dependencies

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log(`Welcome to Holberton School, what is your name?`)
+process.stdin.on("data", (input) => {
+  const text = input.toString().trim();
+  console.log(`"Your name is: ${text}`);
+});
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
