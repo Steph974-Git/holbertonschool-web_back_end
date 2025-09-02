cb3718bd5af7a07062523fd3accea439a9016ab6
Steph974-Git <10466@holbertonstudents.com>
2025-09-02 15:38:58 +0200

    refactor: implement HTTP server to handle student data requests for improved functionality

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const dbPath = process.argv[2];
+
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });;
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const originalLog = console.log;
+      let output = '';
+      console.log = (message) => {
+        output += message + '\n';
+      };
+      await countStudents(dbPath);
+
+            console.log = originalLog;
+      
+      res.end(output);
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
