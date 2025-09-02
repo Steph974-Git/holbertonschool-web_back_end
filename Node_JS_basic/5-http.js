038b42f5a7d7bb44e0776eb5abf7fca76a4e0f29
Steph974-Git <10466@holbertonstudents.com>
2025-09-02 22:20:06 +0200

    refactor: streamline student data handling in HTTP server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,24 +1,35 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const dbPath = process.argv[2];
-
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(dbPath)
-      .then((result) => {
-        res.end(`This is the list of our students\n${result}`);
-      })
-      .catch((err) => {
-        res.end(err.message);
-      });
-  } else {
-    res.end('Hello Holberton School!');
+    return;
   }
+  
+  if (req.url === '/students') {
+    const databasePath = process.argv[2];
+    const originalLog = console.log;
+    let output = '';
+    console.log = (msg) => { output += `${msg}\n`; };
+
+    res.write('This is the list of our students\n');
+
+    countStudents(databasePath)
+      .then(() => {
+        console.log = originalLog;
+        res.end(output.trim());
+      })
+      .catch(() => {
+        console.log = originalLog;
+        res.end('Cannot load the database');
+      });
+    return;
+  }
+  
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
