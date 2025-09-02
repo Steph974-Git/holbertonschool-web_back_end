f22c18a62c6440742e2996fc566159e995b51321
Steph974-Git <10466@holbertonstudents.com>
2025-09-02 15:42:14 +0200

    refactor: clean up formatting and improve code readability in HTTP server implementation

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,22 +1,26 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+
 const dbPath = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });;
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
+    
     try {
       const originalLog = console.log;
       let output = '';
       console.log = (message) => {
         output += message + '\n';
       };
+      
       await countStudents(dbPath);
-
-            console.log = originalLog;
+      
+      console.log = originalLog;
       
       res.end(output);
     } catch (error) {
